ed3f04d669930834cb39d597beaf31ffd5499190
mugisha <mugishaj@outlook.com>
2015-10-28 07:40:51 +0000

    add a show page for each quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,10 +9,18 @@ class QuotesController < ApplicationController
 			flash[:error] = '<strong> Could not save</strong> the data you entered is invalid.'
 		end
 		redirect_to root_path
-
 	end
 
 	def about
+
+	end
+
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+
+		if @quote.blank?
+			render :text => "Not found", :status => :not_found
+		end
 
 	end
 
