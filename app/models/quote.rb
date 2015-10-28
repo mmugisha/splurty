d5fcc4a1c688a5d0306804407a5455d1aa621efd
mugisha <mugishaj@outlook.com>
2015-10-28 06:52:44 +0000

    add unique ID and initials next to the quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,17 @@ class Quote < ActiveRecord::Base
 			:length => {:maximum => 500, :minimum => 3}
 	validates :author, :presence => true, 
 			:length => {:maximum => 50, :minimum => 3}
+
+	def unique_tag
+		#get the initials
+		names_in_array = self.author.split(" ")
+		initials = ""
+		names_in_array.each do |name|
+			initials += name.slice(0).to_s
+		end
+		#get the id of the author
+
+		#return concatnated initials and author
+		return "#{initials}##{self.id}"
+	end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-     assert true
+  test "unique_tag" do
+  	quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+  	quote_id = quote.id.to_s
+  	expected_tag = "KM##{quote_id}"
+  	assert_equal expected_tag, quote.unique_tag
+
   end
 end
